93d418098d4a7b9c994dee4da76b039c94dc5919
bhagyashreegajwe <153532086+bhagyashreegajwe@users.noreply.github.com>
2024-06-11 15:56:38 +0530

    Dist folder commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { IMode } from './models/user.interface';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TDFormTest';
+
+  contcatArr: Array<IMode> = [
+    {
+      moc : 'Email', id:1 
+    },
+    {
+      moc : 'Mobile', id:12
+    },
+  ]
+
+  onSignUp(data:NgForm){
+    if(data.valid){
+      console.log(data)
+      console.log(data.value)
+      data.reset()
+    }
+  }
 }
